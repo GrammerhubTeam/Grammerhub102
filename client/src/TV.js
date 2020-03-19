cb35a85ab752d743300687aa96ce1d2e1b26d315
Steve Smodish <smodish@hotmail.com>
2020-03-18 23:02:22 -0400

    Get it to work

--- client/src/TV.js
+++ client/src/TV.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './TV.css';
 
 const list = [
   "shark",
@@ -8,21 +8,27 @@ const list = [
   "boom!"
 ]
 
-const App = () => {
+const TV = () => {
   return (
     <React.Fragment>
-      <div className="App">
-        <div>
-            <p>TV ROUTE</p>
-            <img src={logo} alt="logo" />
-            <p>I USED TO BE ABLE TO EAT PIZAA ALOT :(</p>
-            <p>
-            Edit <code>src/App.js</code> and save to reload.
-            </p>
-        </div>
+      <div className="bannerlinks">
+        <div className="banner-appletvplus"><img src="https://placekitten.com/80/50" alt="Apple TV+" />Apple TV+</div>
+        <div className="banner-appletvapp"><img src="https://placekitten.com/80/50" alt="Apple TV app" />Apple TV app</div>
+        <div className="banner-appletv4k"><img src="https://placekitten.com/80/50" alt="Apple TV 4K" />Apple TV 4K</div>
+        <div className="banner-appletvhd"><img src="https://placekitten.com/80/50" alt="Apple TV HD" />Apple TV HD</div>
+        <div className="banner-airplay"><img src="https://placekitten.com/80/50" alt="Airplay" />Airplay</div>
+        <div className="banner-accessories"><img src="https://placekitten.com/80/50" alt="Accessories" />Accessories</div>
       </div>
+      <section classname="section-appletvplus">
+        <div classname="scrollingcovers">
+          <p><span>apple</span>tv+</p>
+          <p>Only Apple Originals.</p>
+          <p>Only on Apple TV+.</p>
+          <div classname="button-bar"><button>Watch Now</button><a>Learn more \></a></div>
+        </div>
+      </section>
     </React.Fragment>
   );
 }
 
-export default App;
+export default TV;

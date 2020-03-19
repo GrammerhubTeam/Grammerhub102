ce895bdd33ab1bc91cd387ff9231be0058d00375
Miguel Gomez <mgomez.developer@gmail.com>
2020-03-19 14:58:05 -0400

    Update global footer for all company pages

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -2,9 +2,249 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <p>Footer</p>
-        </div>
+        <footer id="global-footer">
+            <div class="content">
+                <section class="footnote">
+                    <p>Magic Keyboard and Apple Pencil sold separately.</p>
+                </section>
+
+                <nav class="sitemap-links" role="navigation">
+                    <div class="footer-column">
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">Shop and Learn</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="/mac/">Mac</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/ipad/">iPad</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/iphone/">iPhone</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/watch/">Watch</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/tv/">TV</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/music/">Music</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/airpods/">AirPods</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/homepod/">HomePod</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/ipod-touch/">iPod touch</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/buy_accessories">Accessories</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/giftcards">Gift Cards</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="footer-column">
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">Services</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="/apple-music/">Apple Music</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/apple-news/">Apple News+</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/apple-tv-plus/">Apple TV+</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/apple-arcade/">Apple Arcade</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/apple-card/">Apple Card</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/icloud/">iCloud</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">Account</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="https://appleid.apple.com/us/">Manage Your Apple ID</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/account">Apple Store Account</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="https://www.icloud.com">iCloud.com</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="footer-column">
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">Apple Store</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="/retail/">Find a Store</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/retail/geniusbar/">Genius Bar</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/today/">Today at Apple</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/today/camp/">Apple Camp</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/retail/fieldtrip/">Field Trip</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="https://itunes.apple.com/" >Apple Store App</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/special_deals">Refurbished and Clearance</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/payment_plan">Financing</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/trade_in">Apple Trade In</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/order/list">Order Status</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/help">Shopping Help</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="footer-column">
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">For Business</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="/business/">Apple and Business</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/retail/business/">Shop for Business</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">For Education</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="/education/">Apple and Education</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/educationrouting">Shop for College</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">For Healthcare</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="/healthcare/">Apple in Healthcare</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/healthcare/apple-watch/">Health on Apple Watch</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/healthcare/health-records/">Health Records on iPhone</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">For Government</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="/r/store/government/">Shop for Government</a></li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/us/shop/goto/eppstore/veteransandmilitary">Shop for Veterans and Military</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="footer-column">
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">Apple Values</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link" href="/accessibility/">Accessibility</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/education/connectED/">Education</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/environment/">Environment</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/diversity/">Inclusion and Diversity</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/privacy/" data-analytics-title="privacy">Privacy</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/supplier-responsibility/">Supplier Responsibility</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="column-section">
+                            <label class="section-label">
+                                <h3 class="section-title">About Apple</h3>
+                            </label>
+                            <ul class="section-list">
+                                <li class="section-item">
+                                    <a class="section-link">Newsroom</a></li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/leadership/">Apple Leadership</a></li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/jobs/us/">Job Opportunities</a></li>
+                                <li class="section-item">
+                                    <a class="section-link" href="https://investor.apple.com/">Investors</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/apple-events/">Events</a>
+                                </li>
+                                <li class="section-item">
+                                    <a class="section-link" href="/contact/">Contact Apple</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+        </footer>
     )
 }
 

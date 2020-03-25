da087d2bd8f8c80b6b36db19627e499825603100
Steve Smodish <smodish@hotmail.com>
2020-03-24 23:21:03 -0400

    abstracted more data to list, improved css

--- client/src/components/TV/TV.js
+++ client/src/components/TV/TV.js
@@ -1,32 +1,65 @@
-import React from 'react';
-import './TV.css';
+import React from "react";
+import "./TV.css";
 
 const list = [
-  "Apple TV+",
-  "Apple TV app",
-  "Apple TV 4K",
-  "Apple TV HD",
-  "AirPlay",
-  "Accessories"
-]
+  {
+    name: "Apple TV+",
+    tag: "apple-tv-plus",
+    img: "http://placekitten.com/57/70"
+  },
+  {
+    name: "Apple TV app",
+    tag: "apple-tv-app",
+    img: "http://placekitten.com/57/70"
+  },
+  {
+    name: "Apple TV 4K",
+    tag: "apple-tv-4k",
+    img: "http://placekitten.com/57/70"
+  },
+  {
+    name: "Apple TV HD",
+    tag: "apple-tv-hd",
+    img: "http://placekitten.com/57/70"
+  },
+  { name: "AirPlay", tag: "airplay", img: "http://placekitten.com/57/70" },
+  {
+    name: "Accessories",
+    tag: "accessories",
+    img: "http://placekitten.com/57/70"
+  }
+];
 
 const TV = () => {
   return (
     <React.Fragment>
-      <div className="bannerlinks">
-        {list.map(item => <div className="item-button"><img src="https://placekitten.com/80/50" alt="`{item}`" />{item}</div>)}
-      </div>
-      <section classname="section-appletvplus">
+      <nav id="chapternav">
+        <div className="chapternav-wrapper">
+          <ul className="chapternav-items">
+            {list.map(item => (
+              <li className="chapternav-item" id={item.tag}>
+                <img src={item.img} alt={item.name} />
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+      <section className="section-appletvplus">
         <hr />
-        <div classname="scrollingcovers">
-          <p><span>apple</span>tv+</p>
+        <div className="scrollingcovers">
+          <p>
+            <span>apple</span>tv+
+          </p>
           <p>Only Apple Originals.</p>
           <p>Only on Apple TV+.</p>
-          <div classname="button-bar"><button>Watch Now</button><a>Learn more \></a></div>
+          <div className="button-bar">
+            <button id="watch-now-button">Watch Now</button><button id="learn-more-button">Learn more ></button>
+          </div>
         </div>
       </section>
     </React.Fragment>
   );
-}
+};
 
 export default TV;
